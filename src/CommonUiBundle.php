dc4feda0d21ee07248cf279a0451cf22aa709f4f
Vojtěch Bojda <info@vojtechbojda.cz>
2025-10-22 22:29:04 +0200

    updates project, composer install, add symfony console

---
+++ src/CommonUiBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sovic\CommonUi;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CommonUiBundle extends Bundle
+{
+    public function registerCommands(Application $application): void
+    {
+
+    }
+}
