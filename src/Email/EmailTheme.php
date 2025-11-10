bdfc84c1c6f56c5ad8457df2139b91b65469f28b
Vojtěch Bojda <info@vojtechbojda.cz>
2025-11-10 23:59:09 +0100

    email theme

--- src/Email/EmailTheme.php
+++ src/Email/EmailTheme.php
@@ -18,12 +18,14 @@ class EmailTheme implements EmailThemeInterface
     {
         $primaryColor = $this->getPrimaryColor();
         $secondaryColor = $this->getSecondaryColor();
+        $fontFamily = "'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif";
 
         return [
             'primary_color' => $primaryColor,
             'secondary_color' => $secondaryColor,
-            'font_family' => "'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif",
+            'font_family' => $fontFamily,
             'link_style' => 'box-sizing: border-box; color: ' . $primaryColor . '; text-decoration: underline;',
+            'paragraph_style' => 'font-family: ' . $fontFamily . '; font-size: 1rem; font-weight: 400; line-height: 1.5; margin: 0 0 1.5rem; margin-bottom: 0;',
         ];
     }
 }
